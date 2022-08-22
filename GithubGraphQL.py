a9a172b288d4f3b433d65261f44d07332b1e4afb
juancarruthers <juan.carruthers@hotmail.com>
2022-08-22 09:45:57 -0300

    Use threads for concurrency and implement checkpoints

--- GithubGraphQL.py
+++ GithubGraphQL.py
@@ -1,3 +1,6 @@
+import os.path
+import random
+import shutil
 import requests
 import pandas as pd
 import datetime
@@ -6,22 +9,22 @@ import concurrent.futures
 
 def main():
     start = datetime.datetime.now()
-    startSize = 10000
+    startSize, sizeInc, df_data = restoreCheckPoint()
+    savethreshold = 1000
     queryVar = "is:public, language:java, archived:false, mirror:false, forks:>=10, created:<=2021-07-06, size:"
     queryFile = readFile("APIQueries/repositoryMetadata")
     repoCountQueryFile = readFile("APIQueries/repositoryCount")
-    token = readFile("token")
+    tokens = readFile("token").split(",\n")
     repoCountQuery = {'query': repoCountQueryFile, 'variables': {'query': queryVar + ">=" + str(startSize)}}
-    repoCount = makeRequests(repoCountQuery, token)['data']['search']['repositoryCount']
+    repoCount = makeRequest(repoCountQuery, tokens)['data']['search']['repositoryCount']
 
     if repoCount > 0:
-        df_data = []
-        sizeInc = 2000
+        projectsSaved = 0
 
         while repoCount > 0:
 
             repoCountQuery = {'query': repoCountQueryFile, 'variables': {'query': queryVar + str(startSize) + ".." + str(startSize + sizeInc)}}
-            repoCountSubQuery = makeRequests(repoCountQuery, token)['data']['search']['repositoryCount']
+            repoCountSubQuery = makeRequest(repoCountQuery, tokens)['data']['search']['repositoryCount']
 
             j = 1
             while (repoCountSubQuery >= 990) | (repoCountSubQuery == 0):
@@ -30,7 +33,7 @@ def main():
                 else:
                     sizeInc += j
                 repoCountQuery = {'query': repoCountQueryFile, 'variables': {'query': queryVar + str(startSize) + ".." + str(startSize + sizeInc)}}
-                repoCountSubQuery = makeRequests(repoCountQuery, token)['data']['search']['repositoryCount']
+                repoCountSubQuery = makeRequest(repoCountQuery, tokens)['data']['search']['repositoryCount']
                 j += j
 
             repoCount -= repoCountSubQuery
@@ -38,15 +41,15 @@ def main():
             hasNextPage = True
 
             while hasNextPage:
-                elementsPerPage = 30
+                elementsPerPage = 10
                 variables = {'first': elementsPerPage, 'cursor': cursor, 'query': queryVar + str(startSize) + ".." + str(startSize + sizeInc)}
                 repoQuery = {'query': queryFile, 'variables': variables}
-                jsonResponse = makeRequests(repoQuery, token)
+                jsonResponse = makeRequest(repoQuery, tokens)
                 repositories = jsonResponse['data']['search']
 
                 #CONCURRENCY
-                executor = concurrent.futures.ProcessPoolExecutor(max_workers=4)
-                futures = {executor.submit(replaceNestedPropertiesValues, repoProperties, token) for repoProperties in repositories['edges']}
+                executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+                futures = {executor.submit(replaceNestedPropertiesValues, repoProperties, tokens) for repoProperties in repositories['edges']}
                 for future in concurrent.futures.as_completed(futures):
                     repositoryProperties = future.result()
                     df_data.append(repositoryProperties)
@@ -55,24 +58,31 @@ def main():
                 if hasNextPage:
                     cursor = repositories['pageInfo']['endCursor']
 
+            projectsSaved += repoCountSubQuery
+
+            if projectsSaved > savethreshold:
+                saveCheckPoint(startSize, sizeInc, df_data)
+                projectsSaved = 0
+
             startSize += sizeInc
         finish = datetime.datetime.now()
         difference = finish - start
         print('Start:', start, '- Finish:', datetime.datetime.now(), " -  Time:", difference)
         df = pd.DataFrame(df_data)
-        df.to_csv("largerFrame.csv")
+        df.to_csv("./datasets/githubquery" + str(finish.month) + str(finish.year) + ".csv")
+        shutil.rmtree('./.backup')
 
-def replaceNestedPropertiesValues(repoProperties: dict, token: str):
+def replaceNestedPropertiesValues(repoProperties: dict, tokens: str):
     properties = repoProperties['node']
     owner = properties['owner']['login']
     repositoryName = properties['name']
 
     #NORMALIZE OUTPUT
     updateLanguage(properties)
-    updateIssues(properties, token, owner, repositoryName)
-    updatePullRequests(properties, token, owner, repositoryName)
+    updateIssues(properties, tokens, owner, repositoryName)
+    updatePullRequests(properties, tokens, owner, repositoryName)
     updateCommits(properties)
-    updateContributors(properties, token, owner, repositoryName)
+    updateContributors(properties, tokens, owner, repositoryName)
 
     print(datetime.datetime.now(), "- Owner:", owner, "- Repository:", repositoryName)
 
@@ -91,7 +101,7 @@ def updateCommits(json):
     json.pop('defaultBranchRef', None)
     json.update(newJson)
 
-def updateIssues(json, token ,owner, repoName):
+def updateIssues(json, tokens ,owner, repoName):
     if (json['issues']['totalCount'] > 0):
         closed = readFile("APIQueries/Issues/closedIssues")
         open = readFile("APIQueries/Issues/openIssues")
@@ -100,7 +110,7 @@ def updateIssues(json, token ,owner, repoName):
         for state, query in states.items():
             variables = {'owner': owner, 'repoName': repoName}
             lastPullReqQuery = {'query': query, 'variables': variables}
-            jsonResponse = makeRequests(lastPullReqQuery, token)
+            jsonResponse = makeRequest(lastPullReqQuery, tokens)
             pullReqCount = jsonResponse['data']['repository']['issues']['totalCount']
             if pullReqCount > 0:
                 newJson.update({state+"IssuesCount": pullReqCount, state+"IssueLastDate": jsonResponse['data']['repository']['issues']['nodes'][0]['createdAt']})
@@ -115,7 +125,7 @@ def updateIssues(json, token ,owner, repoName):
 1)OBTENER CANTIDAD PULL REQUESTS CERRADAS DEL REPO NO MERGEADAS DE OTRO REPO
 2)FECHA ULTIMO PULL REQUEST
 '''
-def updatePullRequests(json, token, owner, repoName):
+def updatePullRequests(json, tokens, owner, repoName):
     if (json['pullRequests']['totalCount'] > 0):
         closed = readFile("APIQueries/PullReq/closedPullReq")
         merged = readFile("APIQueries/PullReq/mergedPullReq")
@@ -125,7 +135,7 @@ def updatePullRequests(json, token, owner, repoName):
         for state, query in states.items():
             variables = {'owner': owner, 'repoName': repoName}
             lastPullReqQuery = {'query': query, 'variables': variables}
-            jsonResponse = makeRequests(lastPullReqQuery, token)
+            jsonResponse = makeRequest(lastPullReqQuery, tokens)
             pullReqCount = jsonResponse['data']['repository']['pullRequests']['totalCount']
             if pullReqCount > 0:
                 newJson.update({state+"PullReqCount": pullReqCount, state+"PullReqLastDate": jsonResponse['data']['repository']['pullRequests']['nodes'][0]['createdAt']})
@@ -136,43 +146,52 @@ def updatePullRequests(json, token, owner, repoName):
     json.pop('pullRequests', None)
     json.update(newJson)
 
-def updateContributors(json, token, owner, repoName):
+def updateContributors(json, tokens, owner, repoName):
     elementsPerPage = "100"
     url = "https://api.github.com/repos/" + owner + "/" + repoName + "/contributors?per_page="+ elementsPerPage +"&page="
     i = 1
-    response = makeRequests("", token, "GET", url + str(i))
-    acum = 0
+    response = makeRequest("", tokens, "GET", url + str(i))
 
-    while len(response) > 0:
-        acum += len(response)
-        i += 1
-        response = makeRequests("", token, "GET", url + str(i))
+    if not(type(response) is list):
+        acum = 1000
+    else:
+        acum = 0
+
+        while len(response) > 0:
+            acum += len(response)
+            i += 1
+            response = makeRequest("", tokens, "GET", url + str(i))
 
     json.pop('owner', None)
     json.update({"contributors": acum})
 
 
-def makeRequests (query: str | dict, token: str, reqType="POST", url='https://api.github.com/graphql') -> dict:
-    headers = {'Authorization': 'Bearer ' + token}
+def makeRequest (query: str | dict, tokens: list[str], reqType="POST", url='https://api.github.com/graphql') -> dict:
+    headers = {'Authorization': 'Bearer ' + random.choice(tokens)}
     response: dict
+    response, condition = _requestCondition(query, reqType, url, headers)
+
+    while condition:
+        time.sleep(random.choice([50, 100, 150, 200, 250, 300]))
+        response, condition = _requestCondition(query, reqType, url, headers)
+
+    return response
+
+
+def _requestCondition (query: str | dict, reqType: str, url: str, headers: dict) -> tuple[dict, bool]:
     condition: bool
     if reqType == "POST":
         response = requests.post(url, json=query, headers=headers).json()
         condition = response.get("errors", False)
     else:
         response = requests.get(url, headers=headers).json()
-        condition = not(type(response) is list)
-
-    while condition:
-        time.sleep(300)
-        if reqType == "POST":
-            response = requests.post(url, json=query, headers=headers).json()
-            condition = response.get("errors", False)
+        if not (type(response) is list):
+            message: str = response['message']
+            condition = message != 'The history or contributor list is too large to list contributors for this repository via the API.'
         else:
-            response = requests.get(url, headers=headers).json()
-            condition = not(type(response) is list)
+           condition = not (type(response) is list)
 
-    return response
+    return response, condition
 
 
 def readFile(fileName: str):
@@ -184,34 +203,26 @@ def readFile(fileName: str):
 
     return query
 
-''' Dataset con contributors
+def restoreCheckPoint() -> tuple[int, int, list]:
+    startSize = 10000
+    sizeInc = 2000
+    path = './.backup'
+    if os.path.isdir(path):
+        backupData = pd.read_csv(path + '/largerFrame.csv', encoding='unicode_escape').to_dict('records')
+        queryState = pd.read_csv(path + '/queryState.csv')
+        return int(queryState.iloc[0, 0]), int(queryState.iloc[0, 1]), backupData
+    else:
+        return startSize , sizeInc, []
 
+def saveCheckPoint(startSize, sizeInc, dataset):
+    path = "./.backup"
+    if not (os.path.isdir(path)):
+        os.mkdir(path)
 
-def getContributorsTest():
-    df_data = []
-    frame = pd.read_csv('./datasets/Larger/owner.csv')
-    j = 1
-    for index, row in frame.iterrows():
-        url = "https://api.github.com/repos/" + row['url'] + "/contributors?per_page=100&page="
-        headers = {'Authorization': 'Bearer ' + "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"}
-        acum = 0
-        i = 1
-        req = json.loads(requests.get(url + str(i), headers=headers).text)
-
-        while len(req) > 0:
-            if type(req) is list:
-                acum += len(req)
-                i += 1
-            else:
-                time.sleep(300)
-            req = json.loads(requests.get(url + str(i), headers=headers).text)
-
-        print (str(datetime.datetime.now()) + ': ' + str(j) + ' - ' + row['url'] + ' - ' + str(acum))
-        j +=1
-        df_data.append({"contributors": acum})
-    df = pd.DataFrame(df_data)
-    df.to_csv("largerFrame.csv")
-'''
+    df = pd.DataFrame(dataset)
+    df.to_csv(path + '/largerFrame.csv', index=False)
+    queryState = pd.DataFrame([{'startSize': startSize, 'sizeInc': sizeInc}])
+    queryState.to_csv(path + "/queryState.csv", index=False)
 
 if __name__ == '__main__':
     main()
