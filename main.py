69c8e4ccfe10ab7a9e9a20dd2777ba4f72f142c4
juancarruthers <juan.carruthers@hotmail.com>
2023-06-10 20:51:44 -0300

    Main updated to generate datasets for sampling and maintenance experiment

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import datetime
+import numpy as np
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 import SampleBuilder as SB
-import time
 from GithubGraphQL import GithubGraphQL as GQL
-import statsmodels.distributions.empirical_distribution as stMod
 import scipy.stats as sp
 from Maintenance import Maintenance
+
+
 
 def createFrame(queryFilter: str, secondFilter: dict, savethreshold = 5000, p_itemsPageMainQuery = 30):
     today = datetime.date.today()
@@ -19,104 +20,114 @@ def createFrame(queryFilter: str, secondFilter: dict, savethreshold = 5000, p_it
     projectRetriever = GQL(queryFilter, secondFilter, folderPath, p_saveThreshold=savethreshold, p_itemsPageMainQuery=p_itemsPageMainQuery)
     projectRetriever.main()
 
-def updateFrame(folderPath: str, queryFilter: str, secondFilter: dict, dimensions: list[str]):
+'''
+    stochastic equality is readily seen to be equivalent to the p÷ = .5
 
-    projectUpdater = GQL(queryFilter, secondFilter, folderPath)
-    maintenance = Maintenance(projectUpdater, dimensions, secondFilter['dateLastCommit'])
-    frame = pd.read_csv(folderPath + "/frame.csv")
+    In the continuous case p+ > .5 clearly implies that population 1 is
+    stochastically larger than population 2, and p+ < .5 implies that population 1 is
+    stochastically smaller than population 2
 
-    frame = maintenance.updateFrame(frame)
+    Small: 0.56
+    Medium: 0.64
+    Large: 0.71
 
-    frame.to_csv(folderPath + "/frameUpdated.csv", index=False)
-    return frame
+    Vargha, A., & Delaney, H. D. (2000). A critique and improvement of the CL common language effect size statistics of McGraw and Wong. Journal of Educational and Behavioral Statistics, 25(2), 101-132.
+'''
+def varghaDelaney(a, b):
+    m = len(a)
+    n = len(b)
+    U = sp.rankdata(np.concatenate([a, b]))
+    R1 = U[:m].sum()
+    R2 = U[m:].sum()
+    v = (R1/m - (m+1)/2)/n
+    return v
 
-def createStratifiedSample(folderPath: str, dimensions: list[str], sampleSize: int, ksScore = 0.05):
-    start_time = time.time()
-    frame = pd.read_csv(folderPath + "/frame.csv")
-    stratified = pd.DataFrame()
+def representativenessCheck(frame, sample, variables, pvalue) -> bool:
+    dScore, mwScore, ksScore = SB.testSampleDiversityRepresentativeness(frame, sample, variables, pvalue)
+    notRepresentative = False
+    if (len(mwScore[1]) > 0) or (len(ksScore[1]) > 0):
+        notRepresentative = True
 
-    hypRejected = True
-    while hypRejected:
-        stratified, groups = SB.createStratifiedSample(frame, dimensions, sampleSize)
-        for variable in dimensions:
-            cdfFrame = stMod.ECDF(frame[variable].to_numpy())
-            ks = sp.ks_1samp(stratified[variable], cdfFrame)
-            print(variable, ' -Pvalues:', ks[1])
-            if ks[1] > ksScore:
-                hypRejected = False
-            else:
-                hypRejected = True
-                break
+    return notRepresentative
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-    stratified.to_csv(folderPath + "/stratified.csv", index=False)
-    proportion = stratified.shape[0] / frame.shape[0]
-    groupDF = pd.DataFrame(SB.generateGroupsOutput(groups, proportion))
-    groupDF.to_csv(folderPath + "/groups.csv", index=False)
+def samplingExperiment(frame: pd.DataFrame, sampleStrategy: str, dimensions: list[str], samplesPath: str, sampleSize: int, sigLevel:float) -> pd.DataFrame:
 
-def createSimpleRandomSample(folderPath: str, sampleSize: int):
-    frame = pd.read_csv(folderPath + "/frame.csv")
-    simpleRandom = SB.createSimpleRandomSample(frame, sampleSize)
-    simpleRandom.to_csv(folderPath + "/simpleRandom.csv", index=False)
+    sample = pd.DataFrame()
+    notRepresentative = True
+    if sampleStrategy == 'protodash':
+        sample = SB.createProtodashSample(frame, dimensions, sampleSize)
+    if sampleStrategy == 'diverse':
+        sample = SB.createDiverseSample(frame, dimensions)
+    if sampleStrategy == 'stratified':
+        sample = SB.createStratifiedSample(frame, dimensions, sampleSize)[0]
+    if sampleStrategy == 'stratifiedKMeans':
+        while notRepresentative:
+            sample = SB.createKMeansStratifiedSample(frame, dimensions, sampleSize)
+            notRepresentative = representativenessCheck(frame, sample, dimensions, sigLevel)
+    if sampleStrategy == 'simpleRandom':
+        while notRepresentative:
+            sample = SB.createSimpleRandomSample(frame, sampleSize)
+            notRepresentative = representativenessCheck(frame, sample, dimensions, sigLevel)
 
-def createDiverseSample(folderPath: str, dimensions: list[str]):
-    frame = pd.read_csv(folderPath + "/frame.csv")
-    diverse = SB.createDiverseSample(frame, dimensions)
-    diverse.to_csv(folderPath + "/diverse.csv", index=False)
-
-
-def createSamples(folderPath: str, dimensions: list[str]):
-    createSimpleRandomSample(folderPath)
-    createDiverseSample(folderPath, dimensions)
-    createStratifiedSample(folderPath, dimensions)
-
-def updateSampleDTDQ(frame: pd.DataFrame, sample: pd.DataFrame, folderPath: str, dimensions: list[str], queryFilter: str, secondFilter: dict, ksScore= 0.2):
-    projectUpdater = GQL(queryFilter, secondFilter, folderPath)
-    maintenance = Maintenance(projectUpdater, dimensions, secondFilter['dateLastCommit'])
-    sampleDTDQ = maintenance.updateSampleDTDQ(frame, sample, ksScore)
-    sampleDTDQ.to_csv(folderPath + "/sampleUpdatedDTDQ.csv", index=False)
-
-def updateSampleSTDQ(frame: pd.DataFrame, sample: pd.DataFrame, groups: pd.DataFrame, folderPath: str, dimensions: list[str], queryFilter: str, secondFilter: dict, ksScore= 0.2):
-    projectUpdater = GQL(queryFilter, secondFilter, folderPath)
-    maintenance = Maintenance(projectUpdater, dimensions, secondFilter['dateLastCommit'])
-    sampleSTDQ = maintenance.updateSampleST(frame, sample, groups, ksScore, 'dynamic')
-    sampleSTDQ.to_csv(folderPath + "/sampleUpdatedSTDQ.csv", index=False)
-
-def updateSampleSTSQ(frame: pd.DataFrame, sample: pd.DataFrame, groups: pd.DataFrame, folderPath: str, dimensions: list[str], queryFilter: str, secondFilter: dict, ksScore=0.2):
-    projectUpdater = GQL(queryFilter, secondFilter, folderPath)
-    maintenance = Maintenance(projectUpdater, dimensions, secondFilter['dateLastCommit'])
-    sampleSTSQ = maintenance.updateSampleST(frame, sample, groups, ksScore, 'static')
-    sampleSTSQ.to_csv(folderPath + "/sampleUpdatedSTSQ.csv", index=False)
+    return sample
 
 
-def scoreSample(samplePath: str, framePath: str, dimensions: list[str]):
-    sample = pd.read_csv(samplePath)
-    frame = pd.read_csv(framePath)
-    dScore, rScore = SB.testSampleDiversityRepresentativeness(sample, frame, dimensions)
-    nullHypRej = len(rScore[1])
+def maintenanceExperiment(newFrame: pd.DataFrame, sample: pd.DataFrame, maintenanceStrategy: str, dimensions: list[str], samplesPath: str, sampleSize: int, sigLevel: float) -> pd.DataFrame:
+    maintainer = Maintenance(dimensions)
+    sampleMaintained = pd.DataFrame()
 
-    print('Diversity Score:', dScore)
-    for variable in rScore[0]:
-        print(variable['dimension'] + ' - p-value: ' + str(variable['p-value']))
+    if maintenanceStrategy == 'DirectReplacementKM':
+        notRepresentative = True
+        while notRepresentative:
+            sampleMaintained = maintainer.updateSample(newFrame, sample, 'DR', 'kmeans', sampleSize, **{'nClusters': 6})[0]
+            notRepresentative = representativenessCheck(newFrame, sampleMaintained, dimensions, sigLevel)
+    elif maintenanceStrategy == 'DynamicThresholdsKM':
+        notRepresentative = True
+        while notRepresentative:
+            sampleMaintained = maintainer.updateSample(newFrame, sample, 'DT', 'kmeans', sampleSize, **{'nClusters': 6})[0]
+            notRepresentative = representativenessCheck(newFrame, sampleMaintained, dimensions, sigLevel)
+    elif maintenanceStrategy == 'UpdateIfAvailable':
+        projectsUpdated = newFrame[newFrame['id'].isin(sample['id'])]
+        projectsNotUpdated = sample[~sample['id'].isin(newFrame['id'])]
+        sampleMaintained = pd.concat([projectsUpdated, projectsNotUpdated])
+    elif maintenanceStrategy == 'Resample':
+        notRepresentative = True
+        while notRepresentative:
+            sampleMaintained = SB.createKMeansStratifiedSample(newFrame, dimensions, sampleSize)
+            notRepresentative = representativenessCheck(newFrame, sampleMaintained, dimensions, sigLevel)
 
+    if maintenanceStrategy == 'No':
+        sampleMaintained = sample.copy()
 
-def compareWaves():
-    dimensions = ['stargazerCount', 'forkCount', 'closedIssuesCount', 'totalSize', 'closedPullReqCount', 'commits']
+    return sampleMaintained
+def experiment(frame: pd.DataFrame, experimentType: str, strategies: list[str], dimensions: list[str], samplesPath: str, sampleSize = 300, repetitions = 360, sigLevel = 0.05):
 
-    frame1 = pd.read_csv("./datasets/20220715/stratified.csv")
-    frame2 = pd.read_csv("./datasets/20220901/sampleUpdated.csv")
-    frame3 = pd.read_csv("./datasets/20220913/sampleUpdated.csv")
+    emdDataFrame = pd.DataFrame(columns=dimensions)
+    for strategy in strategies:
+        for i in range(repetitions):
+            sample = pd.DataFrame()
+            if experimentType == 'sampling':
+                sample = samplingExperiment(frame, strategy, dimensions, samplesPath, sampleSize, sigLevel)
+                sample.to_csv(f"{samplesPath}{strategy}/s{i}.csv", index=False)
+            elif experimentType == 'maintenance':
+                oldSample = pd.read_csv(f'./datasets/samMainStudy/samples/stratifiedKMeans/s{i}.csv')
+                sample = maintenanceExperiment(frame, oldSample, strategy, dimensions, samplesPath, sampleSize, sigLevel)
+                sample.to_csv(f"{samplesPath}{strategy}/s{i}.csv", index=False)
 
-    for variable in dimensions:
-        res1 = sp.ks_2samp(frame1[variable], frame2[variable])
-        res2 = sp.ks_2samp(frame2[variable], frame3[variable])
-        res3 = sp.ks_2samp(frame1[variable], frame3[variable])
-        print('Comparison 1 & 2 -- dimension: ' + variable + ' - test-score:' + str(res1[0]) + ' - p-value:' + str(
-            res1[1]))
-        print('Comparison 2 & 3 -- dimension: ' + variable + ' - test-score:' + str(res2[0]) + ' - p-value:' + str(
-            res2[1]))
-        print('Comparison 1 & 3 -- dimension: ' + variable + ' - test-score:' + str(res3[0]) + ' - p-value:' + str(
-            res3[1]))
+            print(f'{strategy} Muestra Nº {i}')
+            # compute the Earth Mover's Distance (EMD) between the two distributions
+            emd = []
+            for dimension in dimensions:
+                wassDistance = sp.wasserstein_distance(frame[dimension], sample[dimension])
+                emd.append(wassDistance)
+
+            emdDataFrameAux = pd.DataFrame(emd).T
+            emdDataFrameAux.columns = dimensions
+            emdDataFrame = pd.concat([emdDataFrame, emdDataFrameAux])
+
+        emdDataFrame.to_csv(f'{samplesPath}{strategy}.csv', index=False)
+        emdDataFrame = pd.DataFrame()
+
 
 if __name__ == '__main__':
     today = datetime.date.today()
@@ -126,49 +137,49 @@ if __name__ == '__main__':
     queryFilter = "is:public, language:java, archived:false, mirror:false, forks:>=10, stars:>=10, created:<=" + str(oneYearAgo)
     secondFilter = {'totalSize': 10000, 'closedIssuesCount': 50, 'pullReqCount': 50, 'commits': 1000,
                     'contributors': 0,  'coreContributors': 0, 'history': 0, 'issueFrequency': 0,
-                    'dateLastPullReq': '2010-01-01', 'dateLastCommit': '2010-01-01'}
-
-    folderPath = "./datasets/prueba"
-    dimensions = ['stargazerCount', 'forkCount', 'totalSize', 'commits', 'closedIssuesCount', 'contributors', "pullReqCount"]
-
-    frame = pd.read_csv(folderPath + '/frame.csv')
-    #sampleFolder = './datasets/20220913'
-    #groups = pd.read_csv('./datasets/20220715/groups.csv')
-
-    #createStratifiedSample(folderPath, dimensions, 293)
-    #createSimpleRandomSample(folderPath, 293)
-    #createDiverseSample(folderPath, dimensions)
-    #scoreSample(folderPath + '/simpleRandom.csv', folderPath + '/frame.csv', dimensions)
-    #scoreSample(folderPath + '/diverse.csv', folderPath + '/frame.csv', dimensions)
-    #scoreSample(folderPath + '/stratified.csv', folderPath + '/frame.csv', dimensions)
+                    'dateLastPullReq': '2010-01-01', 'dateLastCommit': '2010-01-01', 'keywords': ['sample', 'tutorial', 'demo', 'conf', 'exam']}
 
     #createFrame(queryFilter, secondFilter)
 
-    projectUpdater = GQL(queryFilter, secondFilter, folderPath)
-    maintenance = Maintenance(projectUpdater, dimensions, secondFilter['dateLastCommit'])
-    sampleSTSQ = maintenance.updateSample(pd.read_csv('./datasets/longStudy/20221206/frame.csv'), pd.read_csv('./datasets/prueba/simpleRandom.csv'), ksScore=0.35)
-    sampleSTSQ.to_csv(folderPath + "/sampleUpdatedSTSQ.csv", index=False)
+    frame1 = pd.read_csv("./datasets/longStudy/20221004/frame.csv")
+    dimensions = ['forkCount', 'stargazerCount', 'totalSize', 'commits', "closedIssuesCount", 'contributors',
+                  "mergedPullReqCount", "closedPullReqCount"]
+    path = "./datasets/samMainStudy/samples/"
+    sampling = ['stratified', 'stratifiedKMeans', 'simpleRandom']
+    experiment(frame1, 'sampling', sampling, dimensions, path)
 
-    '''
-    
+    frame2 = pd.read_csv("./datasets/longStudy/20230301/frame.csv")
+    path2 = "./datasets/samMainStudy/maintenance/"
+    maintenance = ['DirectReplacementKM', 'DynamicThresholdsKM', 'UpdateIfAvailable', 'Resample', 'No']
+    experiment(frame2, 'maintenance', maintenance, dimensions, path2)
 
-    
-    #updateSamples(frame, sampleFolder, groups, folderPath, dimensions, queryFilter, secondFilter, 0.05)
-    updateSampleDTDQ(frame, pd.read_csv(sampleFolder + '/sampleUpdatedDTDQ.csv'), folderPath, dimensions, queryFilter, secondFilter, 0.3)
-    updateSampleSTDQ(frame, pd.read_csv(sampleFolder + '/sampleUpdatedSTDQ.csv'), groups, folderPath, dimensions, queryFilter, secondFilter, 0.05)
-    updateSampleSTSQ(frame, pd.read_csv(sampleFolder + '/sampleUpdatedSTSQ.csv'), groups, folderPath, dimensions, queryFilter, secondFilter, 0.05)
-    
-    
-    folderPath2 = "./datasets/longStudy"
-    sample = folderPath + '/simpleRandom.csv'
 
-    scoreSample(sample, folderPath + '/frame.csv', dimensions)
-    scoreSample(sample, folderPath2 + '/20221004/frame.csv', dimensions)
-    scoreSample(sample, folderPath2 + '/20221017/frame.csv', dimensions)
-    scoreSample(sample, folderPath2 + '/20221101/frame.csv', dimensions)
-    scoreSample(sample, folderPath2 + '/20221115/frame.csv', dimensions)
-    scoreSample(sample, folderPath2 + '/20221206/frame.csv', dimensions)
-    '''
+    for dimension in dimensions:
+        sampleStratified = pd.read_csv("./datasets/samMainStudy/samples/stratified.csv")
+        sampleStratifiedKM = pd.read_csv("./datasets/samMainStudy/samples/stratifiedKMeans.csv")
+        sampleSimpleRandom = pd.read_csv("./datasets/samMainStudy/samples/simpleRandom.csv")
+        mainDRKM = pd.read_csv("./datasets/samMainStudy/maintenance/DirectReplacementKM.csv")
+        mainDTKM = pd.read_csv("./datasets/samMainStudy/maintenance/DynamicThresholdsKM.csv")
+        mainUpdateIfAv = pd.read_csv("./datasets/samMainStudy/maintenance/UpdateIfAvailable.csv")
+        mainResample = pd.read_csv("./datasets/samMainStudy/maintenance/Resample.csv")
+        mainNoUpdate = pd.read_csv("./datasets/samMainStudy/maintenance/No.csv")
+        print(dimension)
+        print(f"Dynamic Thresholds: {sp.mannwhitneyu(mainResample[dimension], mainDTKM[dimension], alternative='less', method='auto')}")
+        print(f"Direct Replacement: {sp.mannwhitneyu(mainResample[dimension], mainDRKM[dimension], alternative='less', method='auto')}")
+        print(f"Diferencia: {sp.mannwhitneyu(mainDTKM[dimension], mainDRKM[dimension], alternative='less', method='auto')}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
