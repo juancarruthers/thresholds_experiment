15045c352c6122dc8dbd015e34cedf833a8efe48
juancarruthers <juan.carruthers@hotmail.com>
2023-11-14 15:20:03 -0300

    Add threads attribute

--- SourceMeter/SourceMeter.py
+++ SourceMeter/SourceMeter.py
@@ -4,17 +4,18 @@ from datetime import datetime
 
 
 class SourceMeter:
-    def __init__(self, resultsDir: str, toolPath: str, projPath: str):
+    def __init__(self, resultsDir: str, toolPath: str, projPath: str, numberOfThreads = 1):
         self._resultsDir = os.path.abspath(resultsDir)
         self._toolPath = os.path.abspath(toolPath)
         self._projectsPath = os.path.abspath(projPath)
+        self._threadsNumber = numberOfThreads
 
     def analyze(self, projectName):
 
         command = f'{self._toolPath} -projectName={projectName} -projectBaseDir={self._projectsPath}/{projectName} -resultsDir={self._resultsDir} ' \
                   f'-runPMD=false -runFB=false -runAndroidHunter=false -runVulnerabilityHunter=false runFaultHunter=false ' \
                   f'-runMetricHunter=false -runRTEHunter=false -runDCF=false -runMET=true -runUDM=false ' \
-                  f'-runLIM2Patterns=false -maximumThreads=1'
+                  f'-runLIM2Patterns=false -maximumThreads={self._threadsNumber}'
 
         try:
             stdout = subprocess.run(command, stdout=subprocess.PIPE, universal_newlines=True, check=True, text=True,
