07488573948e35014e71d8555b404dd36bac4ff8
juancarruthers <juan.carruthers@hotmail.com>
2023-12-11 15:31:14 -0300

    Kmeans clustering approach in sample maintenance

--- Maintenance.py
+++ Maintenance.py
@@ -21,7 +21,7 @@ class Maintenance:
     clustering (DT): nagg y kmeans
     *args nClusters = 6
     '''
-    def updateSample(self, frame:pd.DataFrame, sample: pd.DataFrame, method: str, clustering:str, sampleExpectedSize = 0, **args) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    def updateSample(self, frame:pd.DataFrame, sample: pd.DataFrame, method: str, sampleExpectedSize = 0, **args) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         sampleUpdated = frame[frame['id'].isin(sample['id'])].reset_index(drop=True)
         frameWithoutUpdatedSample = frame[~frame['id'].isin(sampleUpdated['id'])].reset_index(drop=True)
 
@@ -32,11 +32,12 @@ class Maintenance:
         nClusters = args['nClusters']
         if method == 'DR':
             projectsToUpdate = sample[~sample['id'].isin(frame['id'])]
-            sampleUpdated = sample[sample['id'].isin(frame['id'])]
             replacements = self.directReplacement(sampleUpdated, frame, projectsToUpdate)
             sampleUpdated = pd.concat([sampleUpdated, replacements])
-        else:
-            sampleUpdated = self._naggClustering(frame, frameWithoutUpdatedSample, sampleUpdated, sampleSize, method)
+
+        elif method == 'DT':
+            sampleUpdated = self.kMeansThresholds(frame, sampleUpdated, sampleExpectedSize, 'totalSize')
+            #sampleUpdated = self._naggClustering(frame, frameWithoutUpdatedSample, sampleUpdated, sampleSize, method)
 
         '''
         nClusters = args['nClusters']
@@ -123,6 +124,33 @@ class Maintenance:
 
         return sampleUpdated
 
+    def kMeansThresholds(self, frame: pd.DataFrame, sampleUpdated:pd.DataFrame, sampleSize: int, colName= 'totalSize', nClusters=5):
+        numberOfProj = frame.shape[0]
+        popGroups = SB.getGroupsKMeans(frame, colName, nClusters)
+
+        for i in range(len(popGroups)):
+            proportion = round((popGroups[i].shape[0] / numberOfProj) * sampleSize)
+            samGroup = sampleUpdated[sampleUpdated['id'].isin(popGroups[i]['id'])]
+            difference = samGroup.shape[0] - proportion
+            if difference > 0:
+                ids = sampleUpdated[sampleUpdated['id'].isin(samGroup['id'])].sample(difference)
+                sampleUpdated = sampleUpdated[~sampleUpdated['id'].isin(ids)]
+            else:
+                newProj = popGroups[i][~popGroups[i]['id'].isin(sampleUpdated['id'])].sample(difference * -1)
+                sampleUpdated = pd.concat([sampleUpdated, newProj])
+
+        if sampleSize != sampleUpdated.shape[0]:
+            groupSizes = [group.shape[0] for group in popGroups]
+            indexOfLargerGroup = groupSizes.index(max(groupSizes))
+            projects = popGroups[indexOfLargerGroup]
+            if sampleSize < sampleUpdated.shape[0]:
+                id = sampleUpdated[sampleUpdated['id'].isin(projects['id'])].sample(1)['id'].values[0]
+                sampleUpdated = sampleUpdated[sampleUpdated['id'] != id]
+            else:
+                sampleUpdated = pd.concat([sampleUpdated, projects[~projects['id'].isin(sampleUpdated['id'])].sample(1)])
+
+        return sampleUpdated
+
 
     def directReplacement(self, sampleUpdated, frame, projectsToUpdate, order=0):
 
@@ -149,10 +177,10 @@ class Maintenance:
 
             replacements = pd.concat([replacements, pd.DataFrame(similar, columns=columns)])
 
+        for column in replacements.columns:
+            replacements[column] = replacements[column].astype(sampleUpdated[column].dtypes)
+
         return replacements
-
-
-    # NEW REQ
 
     def _naggClustering(self, frame, frameAux, sampleUpdated, sampleSize, method: str):
         diverseSample = SB.createDiverseSample(frame, self._dimensions)
